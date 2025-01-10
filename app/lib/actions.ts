42e1a68f121467c13dba4e9d0e0ea4a1019274cf
tiffanyvoorhees2500 <126093924+tiffanyvoorhees2500@users.noreply.github.com>
2025-01-10 15:46:18 -0700

    made changes to complete lesson 13 it should build in vercel now

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,11 +26,16 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  
+  try{
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  }catch(error){
+    console.error('Database Error: ', error); // Log the error
+    throw new Error('Failed to create new invoice'); // Let Next.js handle the error
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -45,17 +50,27 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
+  try{
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  }catch(error){
+    console.error('Database Error: ', error); // Log the error
+    throw new Error('Failed to update invoice'); // Let Next.js handle the error
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  try{
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  }catch(error){
+    console.error('Database Error: ', error); // Log the error
+    throw new Error('Failed to delete invoice'); // Let Next.js handle the error
+  }
 }
